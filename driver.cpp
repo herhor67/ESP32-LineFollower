dbe1232efc8480dedb48f721fccd6244fe0035da
herhor67 <herhor67@interia.pl>
2022-04-27 12:58:44 +0200

    Update driver.cpp

--- driver.cpp
+++ driver.cpp
@@ -17,54 +17,95 @@
 #include <freertos/FreeRTOS.h>
 #include <freertos/task.h>
 
+// Interrupts
+#include <esp_intr_alloc.h>
+
 // Memory
 #include <esp_heap_caps.h>
-
-static const char *TAG = "Cntrllr🅱 ";
-
 #include <esp_timer.h>
+
+// Classes
+#include "Motor.h"
+//#include "Vehicle.h"
+//
+//
+
+static const char *TAG = "[" __TIME__
+                         "]Cntrllr🅱 ";
+
 double GetTime()
 {
-	return (double)esp_timer_get_time() / 1000;
+    return (double)esp_timer_get_time() / 1000;
+}
+
+static void IRAM_ATTR interrupt_handler(void *arg)
+{
+    static Motor mtrtest;
+    mtrtest.encoderUpdate(gpio_get_level(GPIO_NUM_36), gpio_get_level(GPIO_NUM_39));
+
+    //	ESP_LOGI(TAG, "Encoder: %i", (int)arg);
 }
 
 #ifdef __cplusplus
 extern "C"
 #endif
-	void
-	app_main(void)
+    void
+    app_main(void)
 {
 
-	// gpio_config(&gpio4_conf);
+    ESP_LOGI(TAG, "Wstało!");
 
-	double startTime;
-	uint32_t startTick;
+    PIN_FUNC_SELECT(GPIO_PIN_MUX_REG[12], PIN_FUNC_GPIO);
+    PIN_FUNC_SELECT(GPIO_PIN_MUX_REG[13], PIN_FUNC_GPIO);
 
-	gpio_set_direction(GPIO_NUM_13, GPIO_MODE_OUTPUT);
-	gpio_set_direction(GPIO_NUM_12, GPIO_MODE_OUTPUT);
+    PIN_FUNC_SELECT(GPIO_PIN_MUX_REG[34], PIN_FUNC_GPIO);
+    PIN_FUNC_SELECT(GPIO_PIN_MUX_REG[35], PIN_FUNC_GPIO);
 
-	while (1)
-	{
+    double startTime;
+    uint32_t startTick;
 
-		startTick = esp_log_timestamp();
-		startTime = GetTime();
+    gpio_reset_pin(GPIO_NUM_12);
+    gpio_reset_pin(GPIO_NUM_13);
 
-		gpio_set_level(GPIO_NUM_13, 0);
-		gpio_set_level(GPIO_NUM_12, 1);
+    gpio_set_direction(GPIO_NUM_12, GPIO_MODE_OUTPUT);
+    gpio_set_direction(GPIO_NUM_13, GPIO_MODE_OUTPUT);
 
-		vTaskDelay(pdMS_TO_TICKS(10));
+    gpio_reset_pin(GPIO_NUM_34);
+    gpio_reset_pin(GPIO_NUM_35);
+    gpio_set_direction(GPIO_NUM_34, GPIO_MODE_INPUT);
+    gpio_set_direction(GPIO_NUM_35, GPIO_MODE_INPUT);
+    gpio_pullup_dis(GPIO_NUM_34);
+    gpio_pullup_dis(GPIO_NUM_35);
 
-		gpio_set_level(GPIO_NUM_13, 1);
-		gpio_set_level(GPIO_NUM_12, 0);
+    gpio_install_isr_service(ESP_INTR_FLAG_IRAM | ESP_INTR_FLAG_LEVEL3); // ESP_INTR_FLAG_EDGE
 
-		vTaskDelay(pdMS_TO_TICKS(10));
+    gpio_set_intr_type(GPIO_NUM_34, GPIO_INTR_ANYEDGE);
+    gpio_set_intr_type(GPIO_NUM_35, GPIO_INTR_ANYEDGE);
 
-		// ESP_LOGI(TAG, "Diff: %i ms, %lf ms", esp_log_timestamp() - startTick, GetTime() - startTime);
+    gpio_isr_handler_add(GPIO_NUM_34, interrupt_handler, (void *)GPIO_NUM_34);
+    gpio_isr_handler_add(GPIO_NUM_35, interrupt_handler, (void *)GPIO_NUM_35);
 
-		// ESP_LOGI(TAG, "Diff: %lf ms", GetTime() - startTime);
+    while (1)
+    {
+        startTick = esp_log_timestamp();
+        startTime = GetTime();
 
-		// vTaskDelay(pdMS_TO_TICKS(1000));
-	}
+        // gpio_set_level(GPIO_NUM_12, 0);
+        // gpio_set_level(GPIO_NUM_13, 1);
 
-	return;
+        // vTaskDelay(pdMS_TO_TICKS(1000));
+
+        // gpio_set_level(GPIO_NUM_12, 1);
+        // gpio_set_level(GPIO_NUM_13, 0);
+
+        // vTaskDelay(pdMS_TO_TICKS(1000));
+
+        ESP_LOGI(TAG, "Encoder: %i", mtrtest.ticks);
+
+        //	ESP_LOGI(TAG, "Diff: %i ms, %lf ms", esp_log_timestamp() - startTick, GetTime() - startTime);
+
+        // vTaskDelay(pdMS_TO_TICKS(1000));
+    }
+
+    return;
 }
